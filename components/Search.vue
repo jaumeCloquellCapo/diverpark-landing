5699f4046c765138b9106b31a19813cee840a034
jaume <jaumecloquell@gmail.com>
2020-11-11 15:21:08 +0100

    .

--- components/Search.vue
+++ components/Search.vue
@@ -13,7 +13,7 @@
         <button
           class="bg-blue-500 text-white rounded-full p-2 hover:bg-blue-400 focus:outline-none w-12 h-12 flex items-center justify-center"
         >
-          {{$t('common.search')}}
+          icon
         </button>
       </div>
     </div>
