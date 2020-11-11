4891e9e9dab83f6b0df3f1af7691967332c45613
jaume <jaumecloquell@gmail.com>
2020-11-11 21:44:54 +0100

    .

--- components/HeroContent.vue
+++ components/HeroContent.vue
@@ -8,17 +8,19 @@
         class="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left"
       >
         <h1 class="my-4 text-5xl font-bold leading-tight">
-         {{ $t('common.title') }}
+          {{ $t('common.title') }}
         </h1>
         <p class="leading-normal text-2xl mb-8">
           {{ $t('common.subtitle') }}
         </p>
 
-        <button
-          class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg"
-        >
-          {{ $t('common.booking') }}
-        </button>
+        <NuxtLink :to="localePath('contact')">
+          <button
+            class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg"
+          >
+            {{ $t('common.booking') }}
+          </button>
+        </NuxtLink>
       </div>
       <!--Right Col-->
       <div class="w-full md:w-3/5 py-6 text-center">
