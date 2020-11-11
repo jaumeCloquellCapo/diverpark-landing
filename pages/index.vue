c4c6ecbcd994149a33290c80d745cb3fad1d4365
jaume <jaumecloquell@gmail.com>
2020-11-11 23:31:19 +0100

    .

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,30 @@
 <template>
   <div>
-    <hero />
-    <features />
-    <teasers />
-    <call-to-action />
+    <LazyHydrate when-visible>
+      <hero />
+    </LazyHydrate>
+    <LazyHydrate when-visible>
+      <features />
+    </LazyHydrate>
+    <LazyHydrate when-visible>
+      <teasers />
+    </LazyHydrate>
+    <LazyHydrate when-visible>
+      <call-to-action />
+    </LazyHydrate>
   </div>
 </template>
 
 <script>
-import Hero from '@/components/Hero'
-import Features from '@/components/Features'
-import Teasers from '@/components/Teasers'
-import CallToAction from '@/components/CallToAction'
-
+import LazyHydrate from 'vue-lazy-hydration'
 export default {
   name: 'LandingPage',
   components: {
-    hero: Hero,
-    features: Features,
-    teasers: Teasers,
-    'call-to-action': CallToAction
+    LazyHydrate,
+    hero: () => import('@/components/Hero'),
+    features: () => import('@/components/Features'),
+    teasers: () => import('@/components/Teasers'),
+    'call-to-action': () => import('@/components/CallToAction')
   },
   head() {
     const i18nSeo = this.$nuxtI18nSeo()
@@ -29,18 +34,18 @@ export default {
         {
           hid: 'description',
           name: 'description',
-          content: 'DIVERPARK es una empresa especializada en el Alquiler de Castillos hinchables en Mallorca y fiestas de espuma en Mallorca, además de otros muchos servicios como castillos hinchables acuáticos, servicios de animación infantil, espectáculos de magia y muchas más cosas para diversión de los niños'
+          content:
+            'DIVERPARK es una empresa especializada en el Alquiler de Castillos hinchables en Mallorca y fiestas de espuma en Mallorca, además de otros muchos servicios como castillos hinchables acuáticos, servicios de animación infantil, espectáculos de magia y muchas más cosas para diversión de los niños'
         },
-          {
+        {
           hid: 'og:description',
           name: 'og:description',
-          content: 'Mallorca DIVERPARK es una empresa dedicada al alquiler de castillos hinchables en Mallorca y a la animación infantil en Mallorca entre otros servicios, ofreciendo calidad, compromiso y los mejores servicios para entretener a los niños y que estos se diviertan de forma saludable.'
+          content:
+            'Mallorca DIVERPARK es una empresa dedicada al alquiler de castillos hinchables en Mallorca y a la animación infantil en Mallorca entre otros servicios, ofreciendo calidad, compromiso y los mejores servicios para entretener a los niños y que estos se diviertan de forma saludable.'
         },
         ...i18nSeo.meta
       ],
-      link: [
-        ...i18nSeo.link
-      ]
+      link: [...i18nSeo.link]
     }
   }
 }
